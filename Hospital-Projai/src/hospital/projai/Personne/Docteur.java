7731d7f8abd82728b83fea265021ec299d9bdcd4
louismarquis <louis.marquis@edu.ece.fr>
2018-04-10 10:05:12 +0200

    ROB1
    classe

---
+++ Hospital-Projai/src/hospital/projai/Personne/Docteur.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hospital.projai.Personne;
+
+/**
+ *
+ * @author Louis
+ */
+public class Docteur {
+    
+    private String m_specialite;
+    private int m_batimentDocteur;
+    
+    public Docteur()
+    {
+        
+    }
+    
+    public void setSpecialite(String spe)
+    {
+    m_specialite=spe;
+    }
+    public void setBatimentDoc(int batimentDocteur)
+    {
+    m_batimentDocteur=batimentDocteur;
+    }
+    
+    public String getSpecialite ()
+    {
+        return m_specialite;
+    }
+    
+    public int getBatimentDocteur()
+    {
+        return m_batimentDocteur;
+    }
+    
+    
+    
+    
+}
--- Hospital-Projai/src/hospital/projai/Personne/Personne.java
+++ Hospital-Projai/src/hospital/projai/Personne/Personne.java
@@ -11,4 +11,58 @@ package hospital.projai.Personne;
  */
 public class Personne {
     
+    protected int m_numeroPersonne,m_telephone;
+    protected String m_nom,m_prenom,m_adresse ;
+    
+    
+    public Personne()
+    {
+    
+    
+    }
+    
+   public void setNomPersonne(String nom)
+   {
+       m_nom=nom;
+   }
+   public void setPrenomPersonne(String prenom)
+   {
+       m_prenom=prenom;   
+   }
+   public void setAdresse(String adresse)
+   {
+       m_adresse=adresse;
+   }
+   public void setTelephone(int telephone)
+   {
+       m_telephone=telephone;
+   }
+   public void setNumeroPersonne(int numeropersonne)
+   {
+       m_numeroPersonne=numeropersonne;
+   }
+   
+   
+   public String getNomPersonne()
+   {
+       return m_nom;
+   }
+   public String getPrenomPersonne()
+   {
+       return m_prenom;  
+   }
+   public String getAdresse()
+   {
+       return m_adresse;
+   }
+   public int getTelephone()
+   {
+       return m_telephone;
+   }
+   public int getNumeroPersonne()
+   {
+       return m_numeroPersonne;
+   }
+    
+    
 }
