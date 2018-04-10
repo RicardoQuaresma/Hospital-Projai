00b3e068cd46789375e1d67455cf5026a8f49eb0
RicardoQuaresma <ferreirafe@edu.ece.fr>
2018-04-10 09:44:01 +0200

    V1.2

--- Hospital-Projai/src/hospital/projai/Personne/Personne.java
+++ Hospital-Projai/src/hospital/projai/Personne/Personne.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package hospital.projai;
+package hospital.projai.Personne;
 
 /**
  *
  * @author Ricardo
  */
-public class HospitalProjai {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Personne {
     
 }
