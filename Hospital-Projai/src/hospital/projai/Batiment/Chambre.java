dd828347c19448486ac8e00145367e1eb3d44547
RicardoQuaresma <ferreirafe@edu.ece.fr>
2018-04-10 10:02:32 +0200

    v1.3
    batiment classes getters, setters

---
+++ Hospital-Projai/src/hospital/projai/Batiment/Chambre.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hospital.projai.Batiment;
+
+/**
+ *
+ * @author Ricardo
+ */
+public class Chambre extends Service{
+    
+    private int m_numChambre, m_numSurveillant, m_nbLits;
+    
+    public Chambre()
+    {
+        
+    }
+    
+    public void setNumChambre(int numChambre)
+    {
+        m_numChambre=numChambre;
+    }
+    
+    public void setNumSurveillant(int numSurveillant)
+    {
+        m_numSurveillant=numSurveillant;
+    }
+    
+    public void setNbLits(int nbLits)
+    {
+        m_nbLits=nbLits;
+    }
+    
+    public int getNumChambre()
+    {
+        return m_numChambre;
+    }
+    
+    public int getNumSurveillant()
+    {
+        return m_numSurveillant;
+    }
+    
+    public int getNbLits()
+    {
+        return m_nbLits;
+    }
+    
+}
